9c9d44fb9b1df1fc398c01d36a8fb37759209fa3
emre-buruk007 <emre.buruk007@gmail.com>
2023-11-20 05:57:31 +0200

    Fixed player movement and animation.

--- Scenes/Characters/Scripts/main_character.gd
+++ Scenes/Characters/Scripts/main_character.gd
@@ -2,28 +2,61 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-var move_speed = 170
+@export var move_speed = 130
+var current_direction = "none"
 
-func process_movement_animation(current_vector_values, current_speed):
+func animation_handler(status: String):
+	var dir = current_direction
 	
-	# TODO below line is very ugly animation, fix it later
-	if current_vector_values == Vector2(0,0):
-		animated_sprite_2d.stop()
-	
-	if current_vector_values == Vector2(-1, 0):
-		animated_sprite_2d.play("WalkingLeft")
-	elif current_vector_values == Vector2(1, 0):
-		animated_sprite_2d.play("WalkingRight")
-	elif current_vector_values == Vector2(0, -1):
-		animated_sprite_2d.play("WalkingUp")
-	elif current_vector_values == Vector2(0, 1):
-		animated_sprite_2d.play("WalkingDown")
+	if dir == "right":
+		if status == "moving":
+			animated_sprite_2d.play("WalkingRight")
+		else:
+			animated_sprite_2d.play("IdleRight")
+	elif dir == "left":
+		if status == "moving":
+			animated_sprite_2d.play("WalkingLeft")
+		else:
+			animated_sprite_2d.play("IdleLeft")
+	elif dir == "up":
+		if status == "moving":
+			animated_sprite_2d.play("WalkingUp")
+		else:
+			animated_sprite_2d.play("IdleUp")
+	elif dir == "down":
+		if status == "moving":
+			animated_sprite_2d.play("WalkingDown")
+		else:
+			animated_sprite_2d.play("IdleDown")
 
-func get_input():
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_direction * move_speed
-	process_movement_animation(input_direction, velocity)
+func player_movement(delta):
+	
+	if Input.is_action_pressed("ui_down"):
+		current_direction = "down"
+		animation_handler("moving")
+		velocity.y = move_speed
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_up"):
+		current_direction = "up"
+		animation_handler("moving")
+		velocity.y = -move_speed
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_right"):
+		current_direction = "right"
+		animation_handler("moving")
+		velocity.x = move_speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		current_direction = "left"
+		animation_handler("moving")
+		velocity.x = -move_speed
+		velocity.y = 0
+	else:
+		animation_handler("not moving")
+		velocity.x = 0
+		velocity.y = 0
+	
+	move_and_slide()
 
 func _physics_process(delta):
-	get_input()
-	move_and_slide()
+	player_movement(delta)
